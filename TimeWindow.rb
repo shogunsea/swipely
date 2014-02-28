3ec19e48111639dfab359d6ad6f184eff81fb034
shogunsea <shogunsea@foxmail.com>
2014-02-27 21:55:06 -0500

    add parse multiple&single day range method

--- TimeWindow.rb
+++ TimeWindow.rb
@@ -1,6 +1,8 @@
+# require 'debugger'
+
 class TimeWindow
 	# Define days as class variable 
-	@@days = { :Sun=>"Sun", :Mon=>"Mon", :Tue=>"Tue", :Wed=>"Wed", :Thu=>"Thu", :Fri=>"Fri", :Sat=>"Sat" }
+	@@days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 	def initialize(time)
 		@time_window = parse_multi(time)
@@ -12,24 +14,54 @@ class TimeWindow
 		if time[/\;/]
 			multi_range = time.split(";")
 			multi_range.each do |m|
-				time_window<< parse_single(m)
+				time_window_list<< parse_single(m)
 			end
 		else
-			time_window<<parse_single(time)
+			time_window_list<<parse_single(time)
 		end
 
-		time_window
+		time_window_list
 	end
 
 	def parse_single(time)
-		time_window
-		if time[/\d{4}-\d{4}/]
-		
+		time_window = {} #[]?
+
+		if time[/\D+\-\D+/]
+			day_range = time[/\D+\-\D+/].strip
+			days = day_range.split("-")
+			# Get index from starting day and ending day
+			# debugger
+			start_index = @@days.index days[0]
+			end_index = @@days.index days[1]
+			# Add all days in the day range.
+			if start_index>end_index
+				for i in start_index..6
+					day = @@days[i]
+					# Set default time range.
+					time_window["#{day}"] = { "start" => 000000, "end" => 235959 }
+				end
+				for i in 0..end_index
+					day = @@days[i]
+					# Set default time range.
+					time_window["#{day}"] = { "start" => 000000, "end" => 235959 }
+				end
+
+			else
+				for i in start_index..end_index 
+					day = @@days[i]
+					# Set default time range.
+					time_window["#{day}"] = { "start" => 000000, "end" => 235959 }
+				end
+			end
+
+			# debugger
+			if time[/\d{4}-\d{4}/]
+				time_window.each do |day, time|
+					p "#{day}   #{time}"
+				end
+
+			end
 		end
-
-
-		time[/\D+\-\D+/]
-
 	end
 
 
--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 require './TimeWindow.rb'
 # require 'debugger'
 
-strings = ["", "Fri-Mon", "Sat-Sun; Mon Wed 0700-0900; Thu 0700-0900 1000-1200"]
+strings = ["", "Fri-Mon", "Fri-Mon 0900-1000", "Sat-Sun; Mon Wed 0700-0900; Thu 0700-0900 1000-1200"]
 
 strings.each do |s|
 	a = TimeWindow.new(s)
